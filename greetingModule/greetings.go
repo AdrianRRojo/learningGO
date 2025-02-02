1d76aedc4ef016ada0c4bb6fec7f5fe365d85472
Adrian Rojo <108231637+AdrianRRojo@users.noreply.github.com>
2025-02-02 01:44:23 -0600

    Used slice and math/rand to generate random messages

--- greetingModule/greetings.go
+++ greetingModule/greetings.go
@@ -1,12 +1,18 @@
-package greeings
+package greetings
 
-import "fmt"
-
+import (
+	"fmt"
+	"errors"
+	"math/rand"
+)
 
 //func nameOfFunc(param type) returnType{
-func Hello(name string) string {
-
-	message := fmt.Sprintf("HI, %v. Welcome", name)
+func Hello(name string) (string, error) {
+	
+	if name == "" {
+		return "", errors.New("No name provided")
+	}
+	message := fmt.Sprintf(randomFormat(), name)
 	// := declares a value and it's type in 1 line
 	/*
 		The long way of writing this same line is:
@@ -14,5 +20,24 @@ func Hello(name string) string {
 		message = fmt.Sprintf("HI......", name)
 		$v is filled in using the name variable
 	*/
-	return message
+	return message, nil
 }
+
+func randomFormat() string {
+		// This is a slice, it will change it's size dynamically as you add or remove items
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Nice to meet you, %v!",
+		"YO wazzzzzzup %v!",
+	}
+
+
+		// return a random message.
+		// random index for the slice of formats
+	return formats[rand.Intn(len(formats))]
+
+
+}
+	/* 
+		
+	*/
