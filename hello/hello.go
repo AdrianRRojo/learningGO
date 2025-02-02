26311a89bda24f9fdcc8a0f286a6291711a30fbb
Adrian Rojo <108231637+AdrianRRojo@users.noreply.github.com>
2025-02-02 01:35:29 -0600

    error handling between module and caller

--- hello/hello.go
+++ hello/hello.go
@@ -3,13 +3,25 @@ package main
 import (
 
 	"fmt"
-
+	"log"
 	"example.com/greetings"
 )
 
 
 func main(){
+	//properties of the logger:
+	/*
+		Log prefix: "Greetings: "
+		Flag: 0, disables printing the time, source file and line numbers
+		
+	*/
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 
-	message := greetings.Hello("Adrian")
+	message, err := greetings.Hello("")
+	if err != nil{
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
