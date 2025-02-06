2eb93f23ea0218db010e52ab675611ae58583c0b
Adrian Rojo <rojoadrian16@gmail.com>
2025-02-06 07:05:21 +0000

    added fuzz testing

--- fuzz/main.go
+++ fuzz/main.go
@@ -1,21 +1,30 @@
 package main
 
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+	"unicode/utf8"
+)
 func main() {
 	input := "The quick brown fox jumped over the lazy dog"
-	rev := Reverse(input)
-	doubleRev := Reverse(rev)
+	rev, revErr := Reverse(input)
+	doubleRev, doubleRevErr := Reverse(rev)
 
 	fmt.Printf("Original: %q\n", input)
-	fmt.Printf("Reversed: %q\n", rev)
-	fmt.Printf("Double Rev: %q\n", doubleRev)
+	fmt.Printf("Reversed: %q\n Err: %v \n", rev, revErr)
+	fmt.Printf("Double Rev: %q\n err: %v \n", doubleRev, doubleRevErr)
 }
-func Reverse(s string) string {
-	b := []byte(s)
+func Reverse(s string) (string, error) {
+	//b := []byte(s)
 
-	for i, j := 0, len(b) - 1; i < len(b)/2; i, j = i + 1, j - 1 {
-		b[i], b[j] = b[j], b[i]
+	if !utf8.ValidString(s){
+		return s, errors.New("input is not valid UTF8")
 	}
-	return string(b)
+	r := []rune(s)
+
+	for i, j := 0, len(r) - 1; i < len(r)/2; i, j = i + 1, j - 1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r), nil
 }
--- fuzz/reverse_test.go
+++ fuzz/reverse_test.go
@@ -1,21 +1,36 @@
 package main
 
-import ("testing")
+import (
+	"testing"
+	"unicode/utf8"
+)
 
+func FuzzReverse(f *testing.F) {
 
-func TestReverse(t *testing.T) {
-	testcases := []struct {
-		in, want string
-	}{
-		{"Hello, world", "dlrow ,olleH"},
-		{" ", " "},
-		{"!12345", "54321!"},
-	}
+	testcases := []string{"Hello, world"," ", "12345"}
+
 	for _, tc := range testcases{
-
-		rev := Reverse(tc.in)
-		if rev != tc.want {
-			t.Errorf("Reverse: %q, want %q", rev,tc.want)
-		}
+		f.Add(tc)
 	}
+
+	f.Fuzz(func(t *testing.T, orig string){
+		rev, err1 := Reverse(orig)
+		if err1 != nil{
+			return
+		}
+
+		doubleRev, err2 := Reverse(rev)
+		if err2 != nil{
+			return
+		}
+
+
+		if orig != doubleRev {
+			t.Errorf("Before: %q, \n After: %q \n", orig, doubleRev)
+		}
+
+		if utf8.ValidString(orig) && !utf8.ValidString(rev){
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
 }
